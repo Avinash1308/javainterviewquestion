9adf606e2eba084c57ef028d8c17e5bbc90d82ab
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-05-01 14:25:19 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,14 @@
+public class Main{
+  public static void main(String[] arg){
+    for(int i=1;i<=5;i++){
+      for(int j=1;j<=5+i-1;j++){
+        if((i+j)<=5){
+          System.out.print(' ');
+        }else{
+          System.out.print('*');
+        }
+      }
+      System.out.println();
+    }
+  }
+}
