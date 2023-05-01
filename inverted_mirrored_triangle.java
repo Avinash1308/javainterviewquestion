6c235f36102e2abb6576c8ff8963fe0a9dbb028a
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-05-01 13:52:51 +0530

    Update inverted_mirrored_triangle.java

--- inverted_mirrored_triangle.java
+++ inverted_mirrored_triangle.java
@@ -13,3 +13,24 @@ public class Main{
     }
   }
 }
+
+
+
+
+
+// another method
+
+public class Main{
+  public static void main(String[] arg){
+    for(int i=5;i>=1;i--){
+      for(int j=1;j<=5;j++){
+        if((i+j)<=5){
+          System.out.print(' ');
+        }else{
+          System.out.print('*');
+        }
+      }
+      System.out.println();
+    }
+  }
+}
