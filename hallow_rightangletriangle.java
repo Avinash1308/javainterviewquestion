5bc5144ea05678511d791ec572c6d0e9f1d0e19e
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-05-09 13:42:27 +0530

    Create hallow_rightangletriangle.java

---
+++ hallow_rightangletriangle.java
@@ -0,0 +1,23 @@
+public class Main{
+  public static void main(String[] arg){
+    for (int i=1;i<=5;i++){
+      for(int j=1;j<=i;j++){
+        if(i==5 || (j==1 || j==i)){
+          System.out.print("*");
+        }
+        else{
+          System.out.print(' ');
+        }      
+      }
+      System.out.println();
+    }
+  }
+}
+
+
+//output
+*
+**
+* *
+*  *
+*****
