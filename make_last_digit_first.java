d9ca5b23420fb1a5ab92bf3f1f924044cbadc42b
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-05-11 13:48:58 +0530

    Update make_last_digit_first.java

--- make_last_digit_first.java
+++ make_last_digit_first.java
@@ -2,22 +2,32 @@ import java.util.*;
 public class Main{
   public static void main(String[] arg){
     int x=0;
-    int n=153;
+    int n=9;
     int last=n%10;
     int temp=n;
     String num="";
-    while(temp>0){
-      int digit=temp%10;
-      if(digit!=last){
-        num=Integer.toString(digit)+num;
-      }
-      temp/=10;
+    if(last==n){
+      System.out.print("You have enter one digit number "+last);
+    }
+      
+    else{
+      
+      while(temp>0){
+        int digit=temp%10;
+        if(digit!=last){
+          num=Integer.toString(digit)+num;
+        }
+        temp/=10;
       
       
      
+      }
+    
+    
+      int power=Integer.toString(n).length()-1;
+      x=last*(int) Math.pow(10,power)+Integer.parseInt(num);
+      System.out.print(x);
     }
-    x=last*100+Integer.parseInt(num);
-    System.out.print(x);
   }
 }
 
