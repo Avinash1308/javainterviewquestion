9fb9888417d81cb57c8b4c69a3d3ec244c9155a3
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-05-12 13:55:41 +0530

    Create count_digits.java

---
+++ count_digits.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] arg){
+        int num=123;
+        System.out.print(Integer.toString(num).length());
+    }
+}
+
+
+
+
+//  another approach
+public class Main{
+    public static void main(String[] arg){
+        int num=123;
+        System.out.print((""+num).length());
+    }
+}
