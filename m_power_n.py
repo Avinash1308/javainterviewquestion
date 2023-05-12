bd1626066d9b7c7d1dd907fd53ddf208c90e02fb
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-05-12 13:41:22 +0530

    Create m_power_n.py

---
+++ m_power_n.py
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] arg){
+        int num=2;
+        int pow=5;
+        int result=1;
+        for(int i=1;i<=pow;i++){
+            result=result*num;
+        }
+        System.out.print(result);
+    }
+}
