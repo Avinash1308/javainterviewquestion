f1dea5ec145270dac9f08e516c7442b94ff3b564
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-06-28 14:49:54 +0530

    Update make_last_digit_first.java

--- make_last_digit_first.java
+++ make_last_digit_first.java
@@ -32,3 +32,32 @@ public class Main{
 }
 
 // output --> 315
+
+
+
+
+
+
+
+
+// simple way
+class Main{
+  public static void main(String args[]){
+    int num=153;
+    int last=num%10;
+    int temp=num;
+    if(num==last){
+      System.out.println("single digit number is entered :");
+    }
+    String result="";
+    while(temp>0){
+      int digit=temp%10;
+      if(digit!=last){
+        result=Integer.toString(digit)+result;
+      }
+      temp/=10;
+    }
+    result=last+result;
+    System.out.println(result);
+  }
+}
