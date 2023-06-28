316dd84507018d7465dee2054fab1afd384ac899
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-06-28 14:27:01 +0530

    Update m_power_n.java

--- m_power_n.py
+++ m_power_n.py
@@ -9,3 +9,23 @@ public class Main{
         System.out.print(result);
     }
 }
+
+
+
+
+# another approach
+import java.util.Scanner;
+class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter your number here : ");
+    int num=sc.nextInt();
+    System.out.print("Enter power here : ");
+    int pow=sc.nextInt();
+    int result=1;
+    for(int i=1;i<=pow;i++){
+      result*=num;
+    }
+    System.out.println(result);
+  }
+}
